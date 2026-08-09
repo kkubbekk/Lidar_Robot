fc95efa3966351919c04678015af1f8cd88e8d3f
Kubek <stykajakub@gmail.com>
2026-08-09 16:09:17 +0200

    kok

--- lidar_robot_ws/src/my_bot_description/src/StmParserNode.cpp
+++ lidar_robot_ws/src/my_bot_description/src/StmParserNode.cpp
@@ -15,6 +15,10 @@ class StmParserNode : public rclcpp::Node
             std::bind(&StmParserNode::hardware_callback,this,_1)
             );
 
+            publisher_ = this->create_publisher<nav_msgs::msg::Odometry>(
+                //zaraz sie doda
+            )
+
             RCLCPP_INFO(this->get_logger(),"Parser STM ZOSTAL URUCHOMIONY!");
           }
     private:
@@ -32,7 +36,9 @@ class StmParserNode : public rclcpp::Node
             if (!last_value_subscription) {
             last_value_subscription = msg;
             return;
-        }
+        } 
+          update_odometry();        
+
             last_value_subscription = msg;
           }
 
@@ -42,21 +48,23 @@ class StmParserNode : public rclcpp::Node
           {
             float dt = (msg->time_stamp - last_value_subscription->time_stamp)/1000.0f;
 
+            if(dt<0) return;
 
-            float avg_v_left =  (msg->v_left_mps + laast_value_subscription->v_left_mps)/2.0f;
+
+            float avg_v_left =  (msg->v_left_mps + last_value_subscription->v_left_mps)/2.0f;
             float avg_v_right = (msg->v_right_mps + last_value_subscription->v_right_mps)/2.0f;
 
 
             //narazie czytamy kat z imu pozniej se poczytam o filtrze kalmana i sproboje to dodac z imu oraz enkoder
             // float delta_angle = msg->imu_yaw - last_value_subscription->imu_yaw;
-            float length_drien = (avg_v_left + avg_v_right)/2.0f;
+            float length_driven = (avg_v_left + avg_v_right)/2.0f*dt;
 
             float angular_vel_avg = (msg->motor_angular_speed -laast_value_subscription->motor_angular_speed)/2.0f;
 
-            total_x = l*sin(msg->imu_yaw);
-            total_y = l*cos(msg->imu_yaw);
+            total_x += length_driven*cos(msg->imu_yaw);
+            total_y += length_driven*sin(msg->imu_yaw);
 
-            RCLCPP_INFO(this->get_logger(), "Odometria -> X: %.3f m | Y: %.3f m | Kąt: %.2f rad", total_x, total_y, theta);
+            RCLCPP_INFO(this->get_logger(), "Odometria -> X: %.3f m | Y: %.3f m | Kąt: %.2f rad", total_x, total_y,msg->imu_yaw);
 
             nav_msgs::msg::Odometry odom_msg;
 
@@ -82,6 +90,26 @@ class StmParserNode : public rclcpp::Node
 
           }
 
+          // //jakies wartosci z dupy zeby zobaczyc logike
+          // float p =0.2; 
+          // float q=0.1
+          // float angle =0;
+          // float r = 0.02;
+          // float k_gain;
+          
+          // void kalman_filter_1D(const stm_custom_msgs::msg::StmHardwareState::SharedPtr msg)
+          // {
+          //   //angular
+          //   angle = angle + (msg->motor_angular_velocity*msg->time_stamp);
+          //   p=p+q;
+          //   k_gain = p/(p+r)
+
+          //   angle = angle + K * (msg->imu_yaw - angle);
+          //   p=(1-K)*P;
+            
+          //   //pozniej jakies returnowanie i publioshowanie to przed trzbea bedzie zmienic update odom
+
+          // }
 
 
 };
